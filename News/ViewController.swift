cc92fc778e9c20cd35937407cc29890965498813
shutkin.90 <shutkin.90@mail.ru>
2022-11-19 23:44:49 +0400

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Aleksandr on 19.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
