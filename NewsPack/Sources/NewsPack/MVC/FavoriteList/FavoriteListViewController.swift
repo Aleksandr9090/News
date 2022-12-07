740cca4b4130846f5eaa46001e965de4742f9329
Aleksandr9090 <shutkin.90@mail.ru>
2022-12-08 02:28:04 +0400

    work with favorite list

--- NewsPack/Sources/NewsPack/MVC/FavoriteList/FavoriteListViewController.swift
+++ NewsPack/Sources/NewsPack/MVC/FavoriteList/FavoriteListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-protocol FavoriteViewControllerDelegate {
+protocol FavoriteViewControllerDelegate: AnyObject {
     func reloadData()
 }
 
--- NewsPack/Sources/NewsPack/MVC/FavoriteNews/FavoriteNewsViewController.swift
+++ NewsPack/Sources/NewsPack/MVC/FavoriteNews/FavoriteNewsViewController.swift
@@ -10,7 +10,7 @@ import SnapKit
 
 class FavoriteNewsViewController: UIViewController {
     
-    var delegate: FavoriteViewControllerDelegate?
+    weak var delegate: FavoriteViewControllerDelegate?
         
     var favoriteNews: FavoriteNews?
     
@@ -28,7 +28,7 @@ class FavoriteNewsViewController: UIViewController {
         alpha: 1
     )
     
-    private lazy var newsImage: UIImageView = {
+    private var newsImage: UIImageView = {
         let newsImage = UIImageView()
         newsImage.contentMode = .scaleAspectFit
         newsImage.layer.cornerRadius = newsImage.frame.height / 2
@@ -36,21 +36,21 @@ class FavoriteNewsViewController: UIViewController {
         return newsImage
     }()
     
-    private lazy var nameLabel: UILabel = {
+    private var nameLabel: UILabel = {
         let nameLabel = UILabel()
         nameLabel.font = .systemFont(ofSize: 18, weight: .bold)
         nameLabel.numberOfLines = 0
         return nameLabel
     }()
     
-    private lazy var contentLabel: UILabel = {
+    private var contentLabel: UILabel = {
         let contentLabel = UILabel()
         contentLabel.font = .systemFont(ofSize: 14)
         contentLabel.numberOfLines = 0
         return contentLabel
     }()
     
-    private lazy var authorLabel: UILabel = {
+    private var authorLabel: UILabel = {
         let authorLabel = UILabel()
         authorLabel.font = .systemFont(ofSize: 14)
         authorLabel.numberOfLines = 0
@@ -61,11 +61,6 @@ class FavoriteNewsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
-        view.backgroundColor = .white
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .plain, target: self, action: #selector(deleteButtonTapped))
-        navigationItem.rightBarButtonItem?.tintColor = .red
-
         configure()
         layout()
     }
@@ -82,33 +77,61 @@ class FavoriteNewsViewController: UIViewController {
         contentLabel.text = favoriteNews?.content
         authorLabel.text = favoriteNews?.author
         
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+        view.backgroundColor = .white
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Delete",
+            style: .plain,
+            target: self,
+            action: #selector(deleteButtonTapped)
+        )
+        navigationItem.rightBarButtonItem?.tintColor = .red
+        
         getImage()
     }
     
     private func layout() {
+        newsImageAddSubview()
+        nameLabelAddSubview()
+        contentLabelAddSubview()
+        authorLabelAddSubview()
+    }
+    
+    private func newsImageAddSubview() {
         view.addSubview(newsImage)
+        
         newsImage.snp.makeConstraints { make in
             make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(-40)
             make.leading.equalToSuperview().offset(16)
             make.trailing.equalToSuperview().offset(-16)
             make.height.equalTo(150)
         }
-        
+    }
+    
+    private func nameLabelAddSubview() {
         view.addSubview(nameLabel)
+        
         nameLabel.snp.makeConstraints { make in
             make.top.equalTo(newsImage.snp.bottom).offset(16)
             make.leading.equalToSuperview().offset(16)
             make.trailing.equalToSuperview().offset(-16)
             make.height.equalTo(80)
         }
-        
+    }
+    
+    private func contentLabelAddSubview() {
         view.addSubview(contentLabel)
+        
         contentLabel.snp.makeConstraints { make in
             make.top.equalTo(nameLabel.snp.bottom)
             make.leading.equalToSuperview().offset(16)
             make.trailing.equalToSuperview().offset(-16)
         }
+    }
+    
+    private func authorLabelAddSubview() {
         view.addSubview(authorLabel)
+        
         authorLabel.snp.makeConstraints { make in
             make.top.equalTo(contentLabel.snp.bottom).offset(8)
             make.leading.equalToSuperview().offset(16)
