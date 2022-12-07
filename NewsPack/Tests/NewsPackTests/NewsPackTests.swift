ecc86302961a330822522fc0ceca3a6d9c829b55
Aleksandr9090 <shutkin.90@mail.ru>
2022-12-07 15:16:28 +0400

    add tests file

---
+++ NewsPack/Tests/NewsPackTests/NewsPackTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NewsPack
+
+final class NewsPackTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TestLibrary().text, "Hello, World!")
+    }
+}
